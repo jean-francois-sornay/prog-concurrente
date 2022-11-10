a1e2beedb5dd1a099e86e54d41cf80b9dccf8735
SORNAY Jean-Francois <Jean-Francois.Sornay@bordeaux-inp.fr>
2022-11-10 10:23:32 +0100

    Initial commit for truck project

---
+++ vroum-vroom/src/main/java/com/enseirb/gl/vroumvroom/VroumVroomApplication.java
@@ -0,0 +1,13 @@
+package com.enseirb.gl.vroumvroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroumVroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VroumVroomApplication.class, args);
+	}
+
+}
