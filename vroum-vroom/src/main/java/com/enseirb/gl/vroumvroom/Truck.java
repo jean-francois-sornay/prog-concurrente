7f123437debc671e9ffb3c5b090f6a03b2058fbd
SORNAY Jean-Francois <Jean-Francois.Sornay@bordeaux-inp.fr>
2022-11-10 10:41:33 +0100

    add base app

---
+++ vroum-vroom/src/main/java/com/enseirb/gl/vroumvroom/Truck.java
@@ -0,0 +1,36 @@
+package com.enseirb.gl.vroumvroom;
+
+import java.util.Date;
+
+public class Truck {
+    private String licensePlate;
+    private Date firstCirculationDate;
+    private int emptyWeight;
+
+    public Truck(String licensePlate, Date firstCirculationDate, int emptyWeight) {
+        this.licensePlate = licensePlate;
+        this.firstCirculationDate = firstCirculationDate;
+        this.emptyWeight = emptyWeight;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public Date getFirstCirculationDate() {
+        return firstCirculationDate;
+    }
+
+    public float getEmptyWeight() {
+        return emptyWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Truck { " + "<br>"
+                + "License plate : " + licensePlate + "<br>"
+                + "First circulation : " + firstCirculationDate + "<br>"
+                + "Empty weight : " + emptyWeight + "Kg" + "<br>"
+                + "}";
+    }
+}
