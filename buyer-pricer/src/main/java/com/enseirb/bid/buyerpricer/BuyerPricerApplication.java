66c8d71bc9f5ceb3aae65cc0db0bd63766405ce0
SORNAY Jean-Francois <Jean-Francois.Sornay@bordeaux-inp.fr>
2022-12-02 09:29:04 +0100

    add base bid manager

---
+++ buyer-pricer/src/main/java/com/enseirb/bid/buyerpricer/BuyerPricerApplication.java
@@ -0,0 +1,13 @@
+package com.enseirb.bid.buyerpricer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerPricerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyerPricerApplication.class, args);
+    }
+
+}
---
+++ buyer-pricer/src/test/java/com/enseirb/bid/buyerpricer/BuyerPricerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enseirb.bid.buyerpricer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyerPricerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
